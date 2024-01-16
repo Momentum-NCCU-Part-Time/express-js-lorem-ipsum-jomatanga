c79e10db07fac6af5535ee922781d584f2210b0c
jomatanga <jomatanga11@gmail.com>
2024-01-16 17:15:23 -0500

    WIP

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,26 @@ const config = { port: process.env.PORT || 3000 }
 app.use(cors())
 
 // your API route(s) here
+import { LoremIpsum } from "lorem-ipsum";
 
+// const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+});
+
+lorem.generateParagraphs(7);
+
+app.get('/lorem', (req, res) => {
+  res.json({lorem: lorem.generate})
+})
 
 app.get('*', function (req, res) {
   res.status(404).json({ error: 'route not found' })
